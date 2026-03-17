0ff3abb2e7ed06d6f85f5ef090b32eb5525f171c
adrien04hz <adrien.ahs@gmail.com>
2026-03-17 16:21:56 -0600

    [ADD] JSON e ids

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const asistencias = {
+  data: [
+    { id: 1, nombre: "Preguntas Frecuentes" },
+    { id: 2, nombre: "Productos" },
+    { id: 3, nombre: "Pedidos" },
+    { id: 4, nombre: "Métodos de pago" },
+    { id: 5, nombre: "Envíos" },
+  ],
+};
