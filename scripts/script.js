e6b42b9f9705b8891284e0e637f5e5f42c26552b
adrien04hz <adrien.ahs@gmail.com>
2026-03-17 17:07:59 -0600

    [REFACTOR] Migración de componentes

--- scripts/script.js
+++ scripts/script.js
@@ -107,4 +107,81 @@ async function cargarDatos() {
   contenedorProveedor.innerHTML = html
 }
 
+// Ejecutar la función para cargar los datos al inicio
 cargarDatos()
+
+// Función para navbar
+async function cargarNavbar() {
+  const [generosRes, categoriasRes] = await Promise.all([
+    fetch("../JSON/genero.json"),
+    fetch("../JSON/categorias.json")
+  ]);
+
+  const contenedor = document.getElementById("categoriaNav")
+
+  const categorias = await categoriasRes.json()
+
+  const html = categorias.map(categoria => `
+    <div>
+      <p class="font-semibold mb-3">
+        <a href="#">${categoria.name.toUpperCase()}</a>
+      </p>
+
+      <ul class="space-y-2 text-gray-600">
+        ${categoria.subcategories.map(sub => `
+          <li class="hover:underline cursor-pointer">
+            <a href="#">${sub.nombre}</a>
+          </li>
+        `).join("")}
+      </ul>
+    </div>
+  `).join("")
+
+  contenedor.innerHTML = html
+
+
+  // Contenedor de la UL principal del navbar
+  const container = document.getElementById("principalUL");
+
+  const generos = await generosRes.json()
+
+  const html2 = generos.slice(0, 3).map(genero => `
+    
+    <li class="group hover:underline relative">
+      <a href="#">${genero.nombre.toUpperCase()}</a>
+
+      <!-- Mega menú -->
+      <div class="
+        fixed left-0 top-28 w-full
+        opacity-0 invisible
+        group-hover:opacity-100 group-hover:visible
+        transition-all duration-300
+        bg-white text-black shadow-xl py-10
+      ">
+        <div class="max-w-6xl mx-auto grid grid-cols-3 gap-8 px-8">
+
+          ${categorias.map(categoria => `
+            <div>
+              <p class="font-semibold mb-3">
+                <a href="#">${categoria.name.toUpperCase()}</a>
+              </p>
+
+              <ul class="space-y-2 text-gray-600">
+                ${categoria.subcategories.map(sub => `
+                  <li class="hover:underline cursor-pointer">
+                    <a href="#">${sub.nombre}</a>
+                  </li>
+                `).join("")}
+              </ul>
+            </div>
+          `).join("")}
+
+        </div>
+      </div>
+
+    </li>
+
+  `).join("")
+  
+  container.insertAdjacentHTML("beforeend", html2);
+}
